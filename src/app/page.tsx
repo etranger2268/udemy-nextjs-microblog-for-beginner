902c583c3a1099c587833acaf8eff5debb09284c
etranger2268 <shun.5.2268@gmail.com>
2025-12-30 00:11:10 +0900

    develop-4-16

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Hello, Next Appだよ</h1>;
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-7xl font-bold">
+        Learn to <Link href="/posts/first-post" className="text-blue-500 hover:underline">Next.js!</Link>
+      </h1>
+    </div>
+  );
 }
--- src/app/posts/first-post/page.tsx
+++ src/app/posts/first-post/page.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function FirstPostPage() {
-  return <h1>FirstPost Page</h1>
+  return (
+    <div className="p-4 space-y-4">
+      <h1 className="text-2xl font-bold">最初の投稿</h1>
+      <Link href="/" className="text-blue-500 hover:underline">
+        ホームへ戻る
+      </Link>
+    </div>
+  );
 }
