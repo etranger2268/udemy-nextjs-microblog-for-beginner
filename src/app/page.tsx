290fc7efbc07705373bd7d42381e66052ce0a47e
etranger2268 <shun.5.2268@gmail.com>
2026-01-02 23:55:29 +0900

    develop-5-30

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { PROFILE } from '@/constants';
 import { getPostsData } from '@/lib/post';
 
 export default function Home() {
-  const data = getPostsData();
+  const allPostsData = getPostsData();
 
   return (
     <main className="mt-2 space-y-10">
@@ -15,58 +15,21 @@ export default function Home() {
       <section className="text-center space-y-4">
         <h2 className="text-xl font-semibold">📝エンジニアのブログ</h2>
         <div className="grid flex-wrap max-w-300 gap-10 mx-auto grid-cols-[repeat(auto-fit,minmax(400px,1fr))]">
-          <article>
-            <Link href="/">
-              <Image
-                src="/thumbnail01.jpg"
-                alt="thumbnail01"
-                width={950}
-                height={400}
-                className="mb-5 shadow-xl transition-all duration-300 cursor-pointer hover:translate-0.5 hover:shadow-none"
-              />
-              <h2 className="font-medium">SSGとSSRの使い分けの場面はいつなのか？</h2>
-            </Link>
-            <small>December 30, 2025</small>
-          </article>
-          <article>
-            <Link href="/">
-              <Image
-                src="/thumbnail01.jpg"
-                alt="thumbnail01"
-                width={950}
-                height={400}
-                className="mb-5 shadow-xl transition-all duration-300 cursor-pointer hover:translate-0.5 hover:shadow-none"
-              />
-              <h2 className="font-medium">SSGとSSRの使い分けの場面はいつなのか？</h2>
-            </Link>
-            <small>December 30, 2025</small>
-          </article>
-          <article>
-            <Link href="/">
-              <Image
-                src="/thumbnail01.jpg"
-                alt="thumbnail01"
-                width={950}
-                height={400}
-                className="mb-5 shadow-xl transition-all duration-300 cursor-pointer hover:translate-0.5 hover:shadow-none"
-              />
-              <h2 className="font-medium">SSGとSSRの使い分けの場面はいつなのか？</h2>
-            </Link>
-            <small>December 30, 2025</small>
-          </article>
-          <article>
-            <Link href="/">
-              <Image
-                src="/thumbnail01.jpg"
-                alt="thumbnail01"
-                width={950}
-                height={400}
-                className="mb-5 shadow-xl transition-all duration-300 cursor-pointer hover:translate-0.5 hover:shadow-none"
-              />
-              <h2 className="font-medium">SSGとSSRの使い分けの場面はいつなのか？</h2>
-            </Link>
-            <small>December 30, 2025</small>
-          </article>
+          {allPostsData.map(({ id, data }) => (
+            <article key={id}>
+              <Link href={`posts/${id}`}>
+                <Image
+                  src={data.thumbnail}
+                  alt={data.thumbnail}
+                  width={950}
+                  height={300}
+                  className="mb-5 shadow-xl transition-all duration-300 cursor-pointer hover:translate-0.5 hover:shadow-none"
+                />
+                <h2 className="font-medium">{data.title}</h2>
+              </Link>
+              <small>{data.date}</small>
+            </article>
+          ))}
         </div>
       </section>
     </main>
