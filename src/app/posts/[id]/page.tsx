3d71247c8a071897d6d2310084221e5d9a554b0f
etranger2268 <shun.5.2268@gmail.com>
2026-01-03 00:51:46 +0900

    develop-5-35

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,3 +1,27 @@
-export default function PostPage() {
-  return <h1>動的ルーティング</h1>;
+import { Suspense } from 'react';
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateStaticParams() {
+  return [
+    { id: 'pre-rendering' },
+    { id: 'prerendering-about' },
+    { id: 'react-next' },
+    { id: 'ssg-ssr' },
+  ];
+}
+
+export default function PostPage({ params }: Props) {
+  return (
+    <Suspense fallback={<p>loading...</p>}>
+      <PostPageContent params={params} />
+    </Suspense>
+  );
+}
+
+async function PostPageContent({ params }: Props) {
+  const { id } = await params;
+  return <h2 className="text-xl font-semibold">ID: {id}</h2>;
 }
